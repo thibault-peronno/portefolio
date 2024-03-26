7c1d3398c9c4ff9fc244a7ccf1315219f1e513e3
thibault-peronno <peronnothibault@hotmail.fr>
2024-03-26 15:47:10 +0100

    meurise method

--- docs/conception/MPD/mpd.sql
+++ docs/conception/MPD/mpd.sql
@@ -1,4 +1,8 @@
-CREATE USER IF NOT EXISTS adminPorteFolio IDENTIFIED BY 'porteFolio9854$ThibaultPeronno';
+DROP DATABASE IF EXISTS `porteFolio`;
+DROP USER IF EXISTS 'adminPorteFolio'@'localhost';
+
+CREATE DATABASE IF NOT EXISTS porteFolio CHARACTER SET UTF8;
+USE porteFolio;
 
 DROP TABLE IF EXISTS `organizations`;
 DROP TABLE IF EXISTS `projects`;
@@ -8,18 +12,13 @@ DROP TABLE IF EXISTS `registers`;
 DROP TABLE IF EXISTS `organizations_languages`;
 DROP TABLE IF EXISTS `projects_languages`;
 DROP TABLE IF EXISTS `languages`;
-DROP DATABASE IF EXISTS `porteFolio`;
-DROP USER IF EXISTS 'adminPorteFolio'@'localhost';
 
-
-CREATE DATABASE IF NOT EXISTS porteFolio CHARACTER SET UTF8;
-USE porteFolio;
-CREATE USER 'adminPorteFolio'@'localhost' IDENTIFIED BY 'password';
+CREATE USER IF NOT EXISTS 'adminPorteFolio'@'localhost' IDENTIFIED BY 'porteFolio9854$ThibaultPeronno';
 GRANT ALL PRIVILEGES ON porteFolio.* TO 'adminPorteFolio'@'localhost';
 
 CREATE TABLE `languages`(
 `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-`label` VARCHAR(100) NOT NULL
+`label` VARCHAR(100) NOT NULL,
 `picture` VARCHAR(255) NOT NULL
 );
 
@@ -27,7 +26,12 @@ CREATE TABLE `organizations` (
 `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `title` VARCHAR(100) NOT NULL,
 `description` VARCHAR(100) NOT NULL,
-`picture` VARCHAR(100) NOT NULL,
+`picture` VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE `roles`(
+`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`label` VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE `projects` (
@@ -38,7 +42,7 @@ CREATE TABLE `projects` (
 `organization_id` INT  NOT NULL,
 FOREIGN KEY (`organization_id`)
     REFERENCES `organizations`(`id`)
-    ON DELETE NULL
+    ON DELETE NO ACTION
 );
 
 CREATE TABLE `users`(
@@ -48,13 +52,9 @@ CREATE TABLE `users`(
 `role_id` INT  NOT NULL,
 FOREIGN KEY (`role_id`)
     REFERENCES `roles`(`id`)
-    ON DELETE NULL
+    ON DELETE NO ACTION
 );
 
-CREATE TABLE `roles`(
-`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-`label` VARCHAR(100) NOT NULL,
-);
 
 CREATE TABLE `registers`(
 `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -63,7 +63,7 @@ CREATE TABLE `registers`(
 `user_id` INT NOT NULL,
 FOREIGN KEY (`user_id`)
     REFERENCES `users`(`id`)
-    ON DELETE NULL
+    ON DELETE NO ACTION
 );
 
 CREATE TABLE `organizations_languages`(
@@ -71,11 +71,11 @@ CREATE TABLE `organizations_languages`(
 `organization_id` INT NOT NULL,
 FOREIGN KEY (`organization_id`)
     REFERENCES `organizations`(`id`)
-    ON DELETE NULL,
+    ON DELETE NO ACTION,
 `language_id` INT NOT NULL,
 FOREIGN KEY (`language_id`)
     REFERENCES `languages`(`id`)
-    ON DELETE NULL
+    ON DELETE NO ACTION
 );
 
 CREATE TABLE `projects_languages`(
@@ -83,9 +83,9 @@ CREATE TABLE `projects_languages`(
 `project_id` INT NOT NULL,
 FOREIGN KEY (`project_id`)
     REFERENCES `projects`(`id`)
-    ON DELETE NULL,
+    ON DELETE NO ACTION,
 `language_id` INT NOT NULL,
 FOREIGN KEY (`language_id`)
     REFERENCES `languages`(`id`)
-    ON DELETE NULL
+    ON DELETE NO ACTION
 );
