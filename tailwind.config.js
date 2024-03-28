505e39bead8c43f95f41bff72c08c0d3d8ce4775
thibault-peronno <peronnothibault@hotmail.fr>
2024-03-28 14:26:52 +0100

    tailwind installed and works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js,php, htm}"],
+  content: ["./**/*.{html,js,php,htm}"],
   theme: {
     colors: {
-      'primary': '#d8e8f3',
-      'title': '#3d5a80',
+      transparent: 'transparent',
+      current: 'currentColor',
       'white': '#ffffff',
-      'black': '#000000',
-      'btn-sec': '#293241',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca'
     },
     fontFamily: {
       sans: ['Rubik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#d8e8f3',
+        title: '#3d5a80',
+        white: '#ffffff',
+        black: '#000000',
+        'btn-sec': '#293241',
+      },
+    },
   },
   plugins: [],
 }
