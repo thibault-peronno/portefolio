820915aa2f379a6614f3a02d5c9ae0d1dedae8b8
thibault-peronno <peronnothibault@hotmail.fr>
2024-04-04 11:48:59 +0200

    menu with JS - miss transition

---
+++ public/javascript/app.js
@@ -0,0 +1,10 @@
+// We develop our javascript files on object-oriented programming
+const app = {
+    init: function() {
+        console.log('DOM loaded');
+        menu.init();
+    }
+}
+
+// To load the scripts, we listen an event : DOMContentLoaded
+document.addEventListener("DOMContentLoaded", app.init);
