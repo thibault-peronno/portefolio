84aa06b029123e059809928a7bcba50c8782b554
thibault-peronno <peronnothibault@hotmail.fr>
2024-04-12 09:26:37 +0200

    page connect ok

---
+++ app/Controllers/ConnectController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class ConnectController extends CoreController
+{
+    public function page():void
+    {
+        $this->show('connect');
+    }
+}
---
+++ app/views/connect.tpl.php
@@ -0,0 +1,12 @@
+<div class="flex justify-center items-center">
+<section class="w-9/12 flex flex-col items-center rounded shadow-formConnect py-5 px-2 sm:w-max">
+            <img src="/assets/images/icons/user.svg" alt="" class="w-28 h-28">
+            <form action="" method="post" class="flex flex-col w-full mt-10 sm:w-max">
+                <label for="mail">mail</label>
+                <input type="email" name="mail" id="" class="mt-2.5 mb-5 h-14 rounded border-primary border-2 p-2 sm:w-80">
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="" class="mt-2.5 mb-5 h-14 rounded border-primary border-2 p-2 sm:w-80">
+                <button type="submit" class="bg-btn-sec rounded p-2.5 text-center mt-3 w-full sm:w-80 text-white">Se connecter</button>
+            </form>
+        </section>
+</div>
