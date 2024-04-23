604857362be3a26f950433a1383700ec1fc3bbe0
thibault-peronno <peronnothibault@hotmail.fr>
2024-04-23 13:52:51 +0200

    write addOrga in orga controller up to call model

--- app/Controllers/OrgaController.php
+++ app/Controllers/OrgaController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\Organization;
+
 class OrgaController extends CoreController
 {
     public function organization():void
@@ -10,6 +12,29 @@ class OrgaController extends CoreController
 
     public function addOrgaPage():void
     {
+        $this->boShow('bo-add-orga');
+    }
+
+    public function addOrga():void
+    {
+        dump($_POST);
+        // instancier notre model "organization" pour créer un nouvel objet
+        $organizationModel = new Organization;
+        
+        // échapper nos données pour éviter les failles XSS
+        $title = htmlspecialchars($_POST['title'], ENT_QUOTES);
+        $description = htmlspecialchars($_POST['description'], ENT_QUOTES);
+        $picture = htmlspecialchars($_POST['picture'], ENT_QUOTES);
+
+        // assigner les valeurs à l'objet, pour les récuperer dans notre model.
+        $organizationModel->setTitle($title);
+        $organizationModel->setDescription($description);
+        $organizationModel->setPicture($picture);
+
+        $insert = $organizationModel->addOrganization();
+
+
+        // aller faire la requête dans notre model
         $this->boShow('bo-add-orga');
     }
 }
