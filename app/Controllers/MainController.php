2010d72eb850daa744bf6c6b140264861d514c26
thibault-peronno <peronnothibault@hotmail.fr>
2024-04-23 09:46:40 +0200

    typage method

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -5,18 +5,18 @@ use App\Models\User;
 
 class MainController extends CoreController {
 
-    public function home()
+    public function home(): void
     {
         // echo 'methode home dans MainController';
         $this->show('home');
     }
 
-    public function cv()
+    public function cv(): void
     {
         $this->show('cv');
     }
 
-    public function boHome()
+    public function boHome(): void
     {
         $user = new User;
         // dump('get user', $user->getUser());
