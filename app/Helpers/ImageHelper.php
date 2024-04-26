44483e5f754ced0e2a0cb4820692c51c492c00e9
thibault-peronno <peronnothibault@hotmail.fr>
2024-04-26 10:38:26 +0200

    some comment and type

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -7,25 +7,24 @@ use Error;
 class ImageHelper
 {
     private $IMAGE_TYPES = ['image/jpeg', 'image/png'];
-    // private $IMAGE_SIGN  =['FF D8 FF E3', 'FF D8 FF E0', 'FF D8 FF E8', 'FF D8 FF E0', 'FF D8 FF E1', '89 50 4E 47 0D 0A 1A 0A'];
 
-    private function imageProcess(): bool
+    // To check if I have data in tmp_name and it is not empty. I call the method to check the extension file, and the method to check the sign of the picture.
+    private function imageProcess():bool
     {
-        // vérifier si j'ai une donnée dans tpm_name et que ce n'est pas vide et vérifier l'extension  et la signature de l'image
         return !empty($_FILES['picture']['name']) || $this->checkExtension() || $this->checkSign();
     }
-
-    private function  checkExtension(): bool
+    // To check the extension file.
+    private function  checkExtension():bool
     {
         return !in_array($_FILES['picture']['type'], $this->IMAGE_TYPES);
     }
-
-    private function checkSign(): bool
+    // To check the sign of the picture. One more security.
+    private function checkSign():bool
     {
         return in_array(exif_imagetype($_FILES['picture']['tmp_name']), [IMAGETYPE_JPEG, IMAGETYPE_PNG], true);
     }
 
-    public function languageImage()
+    public function languageImage():bool | Error
     {
         try {
             if (!$this->imageProcess()) {
@@ -35,19 +34,20 @@ class ImageHelper
             if (file_exists(__DIR__ . "/../../public/assets/images/languages/" . $_FILES["picture"]["name"])) {
                 throw new Error("Le fichier existe déjà");
             };
+            // copy the picture from cache to directory I want
             return copy($_FILES['picture']['tmp_name'], "./assets/images/languages/" . $_FILES['picture']['name']);
         } catch (\Throwable $error) {
+            // I return a throw error, to handle in the method where I have called it
             throw $error;
         }
     }
 
-    public function organizationImage()
+    public function organizationImage():bool | Error
     {
         try {
             if (!$this->imageProcess()) {
                 throw new Error("L'image n'est pas valide");
             }
-            // Check if image with same name exist
             if (file_exists(__DIR__ . "/../../public/assets/images/organizations/" . $_FILES["picture"]["name"])) {
                 throw new Error("Le fichier existe déjà");
             };
