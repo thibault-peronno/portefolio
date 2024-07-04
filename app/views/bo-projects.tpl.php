6b90c38ae5abb6d10c7a5eef16d15bb8f5918378
thibault-peronno <peronnothibault@hotmail.fr>
2024-07-04 15:19:30 +0200

    dynamic display projects done

--- app/views/bo-projects.tpl.php
+++ app/views/bo-projects.tpl.php
@@ -1,4 +1,3 @@
-<?php dump($projects); ?>
 <h1 class="text-3xl text-secondary bg-primary text-secondary uppercase font-bold p-2.5 sm:w-[60%] mb-5 sm:mt-12">
   back office Projets
 </h1>
@@ -10,74 +9,27 @@
     </button>
   </a>
   <div class="sm:flex sm:gap-5 sm:flex-wrap">
-    <div class="bg-white rounded border-t-2 border-primary my-5 p-5 sm:w-80">
-      <div class="bg-primary rounded-full w-14 h-14 flex">
-        <img src="/assets/images/icons/edit.svg" alt="" class="m-auto w-10 h-10" />
+    <?php foreach ($projects as $project) : ?>
+      <div class="bg-white rounded border-t-2 border-primary my-5 p-5 sm:w-80">
+        <div class="bg-primary rounded-full w-14 h-14 flex">
+          <img src="/assets/images/icons/edit.svg" alt="" class="m-auto w-10 h-10" />
+        </div>
+        <h2 class="text-2xl text-secondary uppercase my-2">
+          <?= $project['title'] ?>
+        </h2>
+        <div class="flex justify-start gap-2">
+          <?php foreach ($project['labels'] as $label) : ?>
+            <img src="<?= '/assets/images/languages/' . $label['picture'] ?>" alt="" class="w-5" />
+          <?php endforeach ?>
+        </div>
+        <p class="my-2">
+          <?= $project['description'] ?>
+        </p>
+        <button class="bg-btn-sec rounded flex p-2.5 justify-between items-center mt-3 w-full">
+          <p class="text-white">En savoir plus</p>
+          <img src="/assets/images/icons/arrow-right-circle.svg" alt="" />
+        </button>
       </div>
-      <h2 class="text-2xl text-secondary uppercase my-2">
-        Nom du projet
-      </h2>
-      <div class="flex justify-start gap-2">
-        <img src="/assets/images/icons/react_mono.png" alt="" class="w-5" />
-        <img src="/assets/images/icons/javascript_mono.svg" alt="" class="w-5" />
-        <img src="/assets/images/icons/docker_mono.svg" alt="" class="w-5" />
-      </div>
-      <p class="my-2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-        tempora aut et, nesciunt, dolore, debitis deserunt expedita
-        officia ad nam quos qui minima consequuntur illo quasi odio
-        cumque. Voluptatem, tenetur.
-      </p>
-      <button class="bg-btn-sec rounded flex p-2.5 justify-between items-center mt-3 w-full">
-        <p class="text-white">En savoir plus</p>
-        <img src="/assets/images/icons/arrow-right-circle.svg" alt="" />
-      </button>
-    </div>
-    <div class="bg-white rounded border-t-2 border-primary my-5 p-5 sm:w-80">
-      <div class="bg-primary rounded-full w-14 h-14 flex">
-        <img src="/assets/images/icons/edit.svg" alt="" class="m-auto w-10 h-10" />
-      </div>
-      <h2 class="text-2xl text-secondary uppercase my-2">
-        Nom du projet
-      </h2>
-      <div class="flex justify-start gap-2">
-        <img src="/assets/images/icons/react_mono.png" alt="" class="w-5" />
-        <img src="/assets/images/icons/javascript_mono.svg" alt="" class="w-5" />
-        <img src="/assets/images/icons/docker_mono.svg" alt="" class="w-5" />
-      </div>
-      <p class="my-2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-        tempora aut et, nesciunt, dolore, debitis deserunt expedita
-        officia ad nam quos qui minima consequuntur illo quasi odio
-        cumque. Voluptatem, tenetur.
-      </p>
-      <button class="bg-btn-sec rounded flex p-2.5 justify-between items-center mt-3 w-full">
-        <p class="text-white">En savoir plus</p>
-        <img src="/assets/images/icons/arrow-right-circle.svg" alt="" />
-      </button>
-    </div>
-    <div class="bg-white rounded border-t-2 border-primary my-5 p-5 sm:w-80">
-      <div class="bg-primary rounded-full w-14 h-14 flex">
-        <img src="/assets/images/icons/edit.svg" alt="" class="m-auto w-10 h-10" />
-      </div>
-      <h2 class="text-2xl text-secondary uppercase my-2">
-        Nom du projet
-      </h2>
-      <div class="flex justify-start gap-2">
-        <img src="/assets/images/icons/react_mono.png" alt="" class="w-5" />
-        <img src="/assets/images/icons/javascript_mono.svg" alt="" class="w-5" />
-        <img src="/assets/images/icons/docker_mono.svg" alt="" class="w-5" />
-      </div>
-      <p class="my-2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-        tempora aut et, nesciunt, dolore, debitis deserunt expedita
-        officia ad nam quos qui minima consequuntur illo quasi odio
-        cumque. Voluptatem, tenetur.
-      </p>
-      <button class="bg-btn-sec rounded flex p-2.5 justify-between items-center mt-3 w-full">
-        <p class="text-white">En savoir plus</p>
-        <img src="/assets/images/icons/arrow-right-circle.svg" alt="" />
-      </button>
-    </div>
+    <?php endforeach ?>
   </div>
 </section>
