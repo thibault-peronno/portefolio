93b8d1232e1adec0833a9d21967251c0e96bd87b
Thibault PERONNO <peronnothibault@hotmail.fr>
2024-09-11 20:25:59 +0200

    delete bootstrap file : session-start

--- bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-    ini_set('session.save_path', __DIR__ . '/app/temp');
-    ini_set('session.gc_maxlifetime', 3600); // Durée de vie maximale de la session en secondes
-    session_set_cookie_params(3600);
-}
-
-?>
