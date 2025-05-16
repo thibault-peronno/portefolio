02877a2183d6e8a77dc5926cbf7d31d524c90a03
Thibault PERONNO <peronnothibault@hotmail.fr>
2025-05-16 15:38:14 +0200

    fixed css on cv page

--- app/views/cv.tpl.php
+++ app/views/cv.tpl.php
@@ -96,7 +96,7 @@
                         <div class="relative">
                             <div class="hidden lg:block lg:absolute lg:top-0 lg:left-1/2 lg:w-5 lg:h-5 lg:bg-white lg:border-2 lg:border-gray-800 lg:rounded-full lg:translate-x-[-50%] lg:z-10"></div>
                             <div class="relative lg:w-[550px]">
-                                <h3 class="text-xl font-bold text-btn-sec my-5">
+                                <h3 class="text-xl font-bold text-btn-sec mb-5">
                                     Développeur fullstack SmartGlobal Governance
                                 </h3>
                                 <span class="flex flex-row flex-wrap gap-1 mb-2">
@@ -119,7 +119,7 @@
                             <div class="hidden lg:block lg:absolute lg:top-0 lg:left-1/2 lg:w-5 lg:h-5 lg:bg-white lg:border-2 lg:border-gray-800 lg:rounded-full lg:translate-x-[-50%] lg:z-10"></div>
                             <div>
                                 <div class="relative lg:w-[550px]">
-                                    <h3 class="text-xl font-bold text-btn-sec my-5">
+                                    <h3 class="text-xl font-bold text-btn-sec mb-5">
                                         Développeur Front-end iSelection (août 22 - fév 24)
                                     </h3>
                                     <span class="flex flex-row flex-wrap gap-1 mb-2">
@@ -141,7 +141,7 @@
                             </div>
                             <div>
                                 <div class="relative lg:w-[550px]">
-                                    <h3 class="text-xl font-bold text-btn-sec my-5">Bac+3/4 Titre de concepteur et développeur d’application (août 22 - fév 24)</h3>
+                                    <h3 class="text-xl font-bold text-btn-sec mb-5">Bac+3/4 Titre de concepteur et développeur d’application (août 22 - fév 24)</h3>
                                     <span class="flex flex-row flex-wrap gap-1 mb-2">
                                         <p class="text-slate-400 text-xs bg-silver rounded-sm p-1">PHP</p>
                                         <p class="text-slate-400 text-xs bg-silver rounded-sm p-1">Javascript</p>
@@ -167,7 +167,7 @@
                             <div class="hidden lg:block lg:absolute lg:top-0 lg:left-1/2 lg:w-5 lg:h-5 lg:bg-white lg:border-2 lg:border-gray-800 lg:rounded-full lg:translate-x-[-50%] lg:z-10"></div>
                             <div>
                                 <div class="relative lg:w-[550px]">
-                                    <h3 class="text-xl font-bold text-btn-sec my-5">
+                                    <h3 class="text-xl font-bold text-btn-sec mb-5">
                                         Développeur fullstack O’Clock (déc 21- mai 22)
                                     </h3>
                                     <span class="flex flex-row flex-wrap gap-1 mb-2">
@@ -183,7 +183,7 @@
                             </div>
                             <div>
                                 <div class="relative lg:w-[550px]">
-                                    <h3 class="text-xl font-bold text-btn-sec my-5">Bac+2 Titre de développeur web et web mobile (déc 21 - juin 22)</h3>
+                                    <h3 class="text-xl font-bold text-btn-sec mb-5">Bac+2 Titre de développeur web et web mobile (déc 21 - juin 22)</h3>
                                     <span class="flex flex-row flex-wrap gap-1 mb-2">
                                         <p class="text-slate-400 text-xs bg-silver rounded-sm p-1">PHP</p>
                                         <p class="text-slate-400 text-xs bg-silver rounded-sm p-1">Javascript</p>
@@ -202,7 +202,7 @@
                             <div class="relative mb-16">
                                 <div class="hidden lg:block lg:absolute lg:top-0 lg:left-1/2 lg:w-5 lg:h-5 lg:bg-white lg:border-2 lg:border-gray-800 lg:rounded-full lg:translate-x-[-50%] lg:z-10"></div>
                                 <div class="relative lg:w-[550px]">
-                                    <h3 class="text-xl font-bold text-btn-sec my-5">
+                                    <h3 class="text-xl font-bold text-btn-sec mb-5">
                                         Chargé de marketing digital Enfants d’Asie (juin 21-sept 22)
                                     </h3>
                                     <span class="flex flex-row flex-wrap gap-1 mb-2">
@@ -218,8 +218,8 @@
                             <div class="timeline-item opacity-0 translate-y-4 transition-all duration-500 ease-in-out group mb-16">
                                 <div class="relative">
                                     <div class="hidden lg:block lg:absolute lg:top-0 lg:left-1/2 lg:w-5 lg:h-5 lg:bg-white lg:border-2 lg:border-gray-800 lg:rounded-full lg:translate-x-[-50%] lg:z-10"></div>
-                                    <div class="relative lg:w-[550px]">
-                                        <h3 class="text-xl font-bold text-btn-sec my-5">
+                                    <div class="relative lg:w-[550px] lg:left-[51%]">
+                                        <h3 class="text-xl font-bold text-btn-sec mb-5">
                                             Bac+5 Management du marketing & communication digitale École Pégase (sept 17 - juin 19)
                                         </h3>
 
@@ -235,60 +235,5 @@
                     </div>
                 </div>
             </div>
-            <!-- <div class="flex flex-row items-center mt-5 sm:mb-5 sm:mt-12 ">
-            <h1 class="text-3xl text-secondary bg-primary uppercase font-bold p-2.5 w-max rounded">
-                Mon parcours
-            </h1>
-            <a href="https://www.linkedin.com/in/thibault-peronno/ target=" _blank">
-                <img src="/assets/images/icons/linkedin.svg" alt="Link to linkedin profil" class="w-9 h-9 ml-5 cursor-pointer" />
-
-            </a>
-        </div>
-        <p class="text-lg">
-            Issue d'un parcours bac+5 en management du marketing et communication
-            digital et après avoir travaillé quelques temps dans ce domaine, j'ai
-            décidé de me reconvertir dans le développement web.
-        </p> -->
-            <!-- <h2 class="text-2xl text-secondary uppercase mt-14 mb-5">Expériences</h2>
-        <div class="flex flex-wrap justify-between">
-            <div class="sm:w-[45%]">
-                <h3 class="text-xl font-bold text-btn-sec my-5">
-                    Développeur Front-end iSelection (août 22 - fév 24)
-                </h3>
-                <p class="text-lg">
-                    Au sein d’une équipe de 8 personnes, j’étais en charge de la
-                    maintenance, des corrections de bugs et du développement de
-                    nouvelles fonctionnalités. Sur un site en Angular. Réalisation
-                    d’un site en preuve de conception. Base de données en SQL, API en
-                    Laravel, Interface en Vue.Js3 avec Pinia. Ainsi que la mise en
-                    place de tests unitaires et de la sécurité.
-                </p>
-            </div>
-            <div class="sm:w-[45%]">
-                <h3 class="text-xl font-bold text-btn-sec my-5">
-                    Développeur fullstack O’Clock (déc 21- mai 22)
-                </h3>
-                <p class="text-lg">
-                    Simulation de 4 semaines : API Rest avec Laravel et un back-office
-                    avec REACT.
-                </p>
-            </div>
-        </div>
-        <h2 class="text-2xl text-secondary uppercase mt-14 mb-5">Formations</h2>
-        <p class="font-bold">École O’Clock</p>
-        <div class="flex flex-wrap justify-between">
-            <div class="sm:w-[45%]">
-                <h3 class="text-xl font-bold text-btn-sec my-5">Titre de concepteur et développeur d’application (août 22 - fév 24)</h3>
-                <p class="text-lg">PHP, Javascript, VueJS, Dockers, Laravel, NodeJs, ExpressJS et Flutter… sécurité et les tests unitaires.
-                    Titre de développeur web et web mobile (déc 21 - juin 22)
-                    HTML, CSS, Javascript, PHP, Laravel, REACT, WordPress.
-                    challenge : exercices tous les jours après les cours.</p>
-            </div>
-            <div class="sm:w-[45%]">
-                <h3 class="text-xl font-bold text-btn-sec my-5">Titre de développeur web et web mobile (déc 21 - juin 22)</h3>
-                <p class="text-lg">HTML, CSS, Javascript, PHP, Laravel, REACT, WordPress.
-                    challenge : exercices tous les jours après les cours.</p>
-            </div>
-        </div> -->
     </section>
 </div>
